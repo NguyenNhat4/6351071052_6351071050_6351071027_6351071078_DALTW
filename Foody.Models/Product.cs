146b8bdb0b6844d3eac0845f9a667a1b0064fa97
Nhật Nguyễn <minhnhatzzsi4@gmail.com>
2024-10-14 20:34:00 +0700

    Fix | Update
    Fix not being able to create product, update database, product model

--- Foody.Models/Product.cs
+++ Foody.Models/Product.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,8 +15,10 @@ namespace Foody.Models
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
-        public string Description { get; set; }
-       
+
+
+        public string? Description { get; set; }
+
         [Required]
         [Display(Name = "List Price")]
         [Range(1, 1000)]
@@ -35,9 +38,11 @@ namespace Foody.Models
         [Display(Name = "Price for 100+")]
         [Range(1, 1000)]
         public double Price100 { get; set; }
-       
+        [ForeignKey(nameof(Category))]
+        [Required]
         public int CategoryId { get; set; }
-        public string imageUrl { get; set; }
+        [Url]
+        public string? imageUrl { get; set; }
 
 
 
