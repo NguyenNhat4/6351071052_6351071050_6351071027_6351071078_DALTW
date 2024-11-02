4c0c0ca372dc2bbfd53c411c8a44e54994a39892
Nhật Nguyễn <minhnhatzzsi4@gmail.com>
2024-11-02 13:13:08 +0700

    Add
    add new repository to unitofwork

--- Foody.DataAccess/Repository/UnitOfWork.cs
+++ Foody.DataAccess/Repository/UnitOfWork.cs
@@ -16,7 +16,8 @@ namespace Foody.DataAccess.Repository
 
         public IProductRepository Product { get; private set; }
         public IShoppingCartRepository ShoppingCart { get; private set; }
-
+        public IOrderDetailRepository OrderDetail { get; private set; }
+        public IOrderHeaderRepository OrderHeader { get; private set; }
         public IApplicationUserRepository ApplicationUser { get; private set; }
         private ApplicationDbContext _db;
 
