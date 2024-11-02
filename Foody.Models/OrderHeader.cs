0997193fa7f9221b305f1ace9d268966d2ea3147
Nhật Nguyễn <minhnhatzzsi4@gmail.com>
2024-11-03 00:36:27 +0700

    Update
    Update migration

--- Foody.Models/OrderHeader.cs
+++ Foody.Models/OrderHeader.cs
@@ -18,12 +18,12 @@ namespace Foody.Models
         public ApplicationUser ApplicationUser { get; set; }
         public DateTime OrderDate { get; set; }
         public DateTime ShippingDate { get; set; }
-        public double OrderTotal { get; set; }
-        public string TrackingNumber { get; set; }
-        public string Carrier { get; set; }
-        public string PaymentStatus { get; set; }
-        public string OrderStatus { get; set; }
-        public string PaymentMethod { get; set; }
+        public double  OrderTotal { get; set; }
+        public string? TrackingNumber { get; set; }
+        public string? Carrier { get; set; }
+        public string? PaymentStatus { get; set; }
+        public string? OrderStatus { get; set; }
+        public string? PaymentMethod { get; set; }
         public DateTime PaymentDate { get; set; }
         public DateTime PaymentDueDate { get; set; }
         public string? PaymentIntentId { get; set; }
